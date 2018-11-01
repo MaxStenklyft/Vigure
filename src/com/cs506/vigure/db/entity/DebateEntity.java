63c998eb109a1b0ebc72d93de407972d1a29befa
Max Stenklyft <maxstenklyft@Maxs-MacBook-Pro-2.local>
2018-10-31 21:01:25 -0500

    reseting

---
+++ src/com/cs506/vigure/db/entity/DebateEntity.java
@@ -0,0 +1,37 @@
+package com.cs506.vigure.db.entity;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="debate_model")
+public class DebateEntity { // this represents the debate_model table in the db
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ID")
+	private long id;
+	
+	@Column(name="topic")
+	private String topic;
+	
+	@Column(name="status")
+	private String status;
+	
+	@Column(name="instigatorNum")
+	private long instigatorNum;
+	
+	@Column(name="defenderNum")
+	private long defenderNum;
+	
+	@Column(name="startTime")
+	private Date startTime;
+	
+	@Column(name="timeInterval")
+	private int timeInterval;
+}
