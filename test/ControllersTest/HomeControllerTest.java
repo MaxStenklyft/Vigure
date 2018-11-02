487710d9c4b5577ebb8557a035bebd23a8893480
Cam <Cam@172.16.5.121>
2018-11-01 23:08:02 -0500

    HomeControllerTest implemented.

--- test/ControllersTest/HomeControllerTest.java
+++ test/ControllersTest/HomeControllerTest.java
@@ -1,14 +1,20 @@
 package ControllersTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import com.cs506.vigure.Controllers.HomeController;
 
 class HomeControllerTest {
 
 	@Test
 	void testLoadLoginPage() {
-		fail("Not yet implemented");
+		// getting instance of a HomeController
+		HomeController homeControllerInstance = new HomeController();
+
+		// getting return string from loadLoginPage method
+		String redirect = homeControllerInstance.loadLoginPage();
+				
+		assertEquals(redirect, "home");
 	}
 
 }
