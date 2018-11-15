a1b6dfe4a1b41e2d1d93cb87e2077aeefb38d68a
Nick Vander Heyden <vandy.nick@gmail.com>
2018-11-14 18:29:32 -0600

    Updating login procedure to work with hashed passwords

--- src/com/cs506/vigure/Controllers/AccountController.java
+++ src/com/cs506/vigure/Controllers/AccountController.java
@@ -1,5 +1,8 @@
 package com.cs506.vigure.Controllers;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
@@ -32,7 +35,10 @@ public class AccountController {
 		@RequestParam("password") String password,
 		HttpSession session) {
 		
-		if(loginDAO.validateUser(username, password)) {
+		String hashedPassword = hashPassword(password);
+		password = null;
+		
+		if(hashedPassword != null && loginDAO.validateUser(username, hashedPassword)) {
 			session.setAttribute("username", username);
 			return "main";
 		}
@@ -46,5 +52,35 @@ public class AccountController {
 	public String logout(HttpSession session) {
 		session.removeAttribute("username");
 		return "redirect:../";
+	}
+	
+	private String hashPassword(String password) {
+		String hashedPassword = null;
+		
+		try {
+			// begin password hashing
+			// Create MessageDigest instance for MD5
+	        MessageDigest md = MessageDigest.getInstance("MD5");
+	        //Add password bytes to digest
+	        md.update(password.getBytes());
+	        //Get the hash's bytes
+	        byte[] bytes = md.digest();
+	        //This bytes[] has bytes in decimal format;
+	        //Convert it to hexadecimal format
+	        StringBuilder sb = new StringBuilder();
+	        for(int i=0; i< bytes.length ;i++)
+	        {
+	            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	        }
+	        //Get complete hashed password in hex format
+	        hashedPassword = sb.toString();	
+	        password = null;
+		}
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+		
+		return hashedPassword;
 	}
 }
