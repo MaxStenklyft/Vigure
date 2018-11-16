b051bda8abadbf88fa9904a997bb4508cff857e3
Nick Vander Heyden <vandy.nick@gmail.com>
2018-11-15 21:07:57 -0600

    loadLoginPage() Test

---
+++ test/ControllersTest/CreateAccountControllerTest.java
@@ -0,0 +1,17 @@
+package ControllersTest;
+
+import org.junit.jupiter.api.Test;
+import com.cs506.vigure.Controllers.CreateAccountController;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CreateAccountControllerTest {
+
+	@Test
+	void testLoadLoginPage() {
+		CreateAccountController cac = new CreateAccountController();
+		
+		String result = cac.loadLoginPage();
+		
+		assertEquals("createAccount", result);
+	}
+}
