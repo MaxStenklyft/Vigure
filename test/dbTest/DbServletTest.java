1dddf8365f98544d5bc05f70a8bb6b853d0979d1
Nick Vander Heyden <vandy.nick@gmail.com>
2018-11-15 21:01:52 -0600

    DbServletTest framework
    Will need to later on get Mockito setup to implement this.

---
+++ test/dbTest/DbServletTest.java
@@ -0,0 +1,14 @@
+package dbTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import com.cs506.vigure.db.DbServlet;
+
+public class DbServletTest {
+	
+	
+	@Test
+	void testDoGetBoolean() {
+		fail("Unimplemented");
+	}
+}
