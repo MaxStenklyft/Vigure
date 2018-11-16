a49004b7ee047d26c9dd7f10e235460e03ceaa5c
Max Stenklyft <maxstenklyft@Maxs-MacBook-Pro-2.local>
2018-11-16 17:31:36 -0600

    started working on OtherUserController

--- src/com/cs506/vigure/Controllers/OtherUserPageController.java
+++ src/com/cs506/vigure/Controllers/OtherUserPageController.java
@@ -2,6 +2,10 @@ package com.cs506.vigure.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.cs506.vigure.db.dao.UserDAO;
 import com.cs506.vigure.db.entity.UserEntity;
@@ -15,9 +19,11 @@ public class OtherUserPageController {
 	@Autowired
 	private UserDAO userDAO;
 	
-	
-	public void loadUserPage() {
-		//TODO
+	@RequestMapping(value = "/user/{username}", method = RequestMethod.GET)
+	public ModelAndView loadUserPage( @PathVariable("username") long username) {
+		ModelAndView mav = new ModelAndView("other");
+		
+		return mav;
 	}
 	
 	public void loadDebateCreationModel() {
