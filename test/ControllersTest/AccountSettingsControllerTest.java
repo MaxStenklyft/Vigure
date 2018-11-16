d2bf5f2de78352d0f4d001c02cd982091867df0a
Nick Vander Heyden <vandy.nick@gmail.com>
2018-11-15 20:11:07 -0600

    Updating DebateRoomControllerTest and AccountSettingsControllerTest
    More code coverage :)

--- test/ControllersTest/AccountSettingsControllerTest.java
+++ test/ControllersTest/AccountSettingsControllerTest.java
@@ -4,11 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.cs506.vigure.Controllers.AccountSettingsController;
+import com.cs506.vigure.Controllers.HomeController;
+
 class AccountSettingsControllerTest {
 
 	@Test
 	void testLoadUserSettings() {
-		fail("Not yet implemented");
+		// getting instance of a HomeController
+		AccountSettingsController accSettControllerInstance = new AccountSettingsController();
+
+		// getting return string from loadLoginPage method
+		String redirect = accSettControllerInstance.loadUserSettingsPage();
+				
+		assertEquals(redirect, "settings");	
 	}
 
 	@Test
